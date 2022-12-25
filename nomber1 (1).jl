7f7dfed2f20d8ab77820c819c97475256b1d0f9f
Suva0303 <victor.lipko.99@mail.ru>
2022-12-25 19:55:04 +0300

    number 1(1)
    number 1(1)

---
+++ nomber1 (1).jl
@@ -0,0 +1,27 @@
+using HorizonSideRobots
+
+function suva_cccc!(r::Robot)
+    for side in (Nord, Sud, West, Ost)
+        n_steps = putmarkers_until_border!(r, side)
+        moves!(r, inverse_side(side), n_steps)
+    end
+    putmarker!(r)
+end
+
+
+function putmarkers_until_border!(r::Robot, side::HorizonSide)::Int
+    n_steps = 0
+    while !isborder(r, side) 
+        move!(r, side)
+        putmarker!(r)
+        n_steps += 1
+    end 
+    return n_steps
+end
+
+function inverse_side(side::HorizonSide)::HorizonSide
+    inv_side = HorizonSide((Int(side) + 2) % 4)
+    return inv_side
+end
+
+suva_cccc!(Robot("pole69.sit", animate=true))
---
+++ testpractic1.1.jl
@@ -0,0 +1,4 @@
+using HorizonSideRobots
+include("practic 1.jl")
+r=Robot("1.sit"; animate=true)
+suva_cccc!(r)
